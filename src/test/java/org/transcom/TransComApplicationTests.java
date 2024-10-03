6a2239611297a2d2cc53df661344f54960e88f2b
Aleksandr <dragin.alex@gmail.com>
2024-10-03 22:04:44 +0200

    init commit

---
+++ src/test/java/org/transcom/TransComApplicationTests.java
@@ -0,0 +1,13 @@
+package org.transcom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransComApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

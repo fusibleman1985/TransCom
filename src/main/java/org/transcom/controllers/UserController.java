44ff36acf1c4330be9e537707633383ac4ea8e67
Aleksandr <dragin.alex@gmail.com>
2024-11-11 10:20:29 +0100

    remake DTO + update UserServiceImpl

--- src/main/java/org/transcom/controllers/UserController.java
+++ src/main/java/org/transcom/controllers/UserController.java
@@ -4,7 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.transcom.dto.UserDto;
+import org.transcom.dto.UserDtoRequest;
 import org.transcom.dto.UserDtoResponse;
 import org.transcom.services.UserService;
 
@@ -19,8 +19,8 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping
-    public ResponseEntity<UserDtoResponse> registerUser(@RequestBody UserDto userDto) {
-        UserDtoResponse resultUser = userService.createUser(userDto);
+    public ResponseEntity<UserDtoRequest> registerUser(@RequestBody UserDtoRequest userDto) {
+        UserDtoRequest resultUser = userService.createUser(userDto);
         return ResponseEntity.status(HttpStatus.OK).body(resultUser);
     }
 
@@ -31,20 +31,20 @@ public class UserController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<UserDtoResponse> getUserById(@PathVariable UUID id) {
-        UserDtoResponse user = userService.findUserById(id);
+    public ResponseEntity<UserDtoRequest> getUserById(@PathVariable UUID id) {
+        UserDtoRequest user = userService.findUserById(id);
         return ResponseEntity.ok(user);
     }
-//
-//    @PutMapping("/{id}")
-//    public ResponseEntity<UserDtoResponse> updateUser(@PathVariable UUID id, @RequestBody User user) {
-//        User resultUser = userService.updateUser(user);
-//        return ResponseEntity.ok(resultUser);
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    public ResponseEntity<Void> deleteUser(@PathVariable UUID id) {
-//        userService.deleteUser(id);
-//        return ResponseEntity.noContent().build();
-//    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserDtoRequest> updateUser(@PathVariable UUID id, @RequestBody UserDtoRequest userDto) {
+        UserDtoRequest updatedUser = userService.updateUser(id, userDto);
+        return ResponseEntity.ok(updatedUser);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable UUID id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
 }
